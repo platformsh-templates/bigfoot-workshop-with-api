9797e890fb186034b130b4c843a4d93a4f45a9ff
Florent Huck <flovntp@gmail.com>
2023-06-08 12:45:15 +0200

    adding all CRUD operation on sightings

--- api/src/Entity/BigFootSighting.php
+++ api/src/Entity/BigFootSighting.php
@@ -8,6 +8,7 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Post;
 use App\Repository\BigFootSightingRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -23,7 +24,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
         new Put(),
         new Patch(),
         new Delete(),
-        new GetCollection(),
         new Post(),
     ],
     mercure: true,
